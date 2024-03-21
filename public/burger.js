305d04c218d7774b4c67f1af4c2d0cdf1533848f
sara gronkjaer <sagr0001@stud.kea.dk>
2024-03-21 12:06:26 +0100

    burgermenu

--- public/burger.js
+++ public/burger.js
@@ -1,16 +1,4 @@
-const burger = document.querySelector(".burger");
-const nav = document.querySelector("nav");
-const menu = document.querySelector(".menu");
-const links = document.querySelectorAll(".menu li a");
-
-burger.addEventListener("click", () => {
-  burger.classList.toggle("active");
-  nav.classList.toggle("active");
-});
-
-links.forEach((link) => {
-  link.addEventListener("click", () => {
-    burger.classList.remove("active");
-    nav.classList.remove("active");
-  });
+document.querySelector(".burger").addEventListener("click", function () {
+  document.querySelector(".global_menu").classList.toggle("active");
+  document.querySelector(".lokal_menu").classList.toggle("active");
 });
